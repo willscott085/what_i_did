c1728c4632e110ed5f277db13eab88afb39c9441
Will Scott <willscott.dev@gmail.com>
2025-10-05 15:58:46 +0100

    Added tailwind v4 and zod

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
-import viteReact from '@vitejs/plugin-react'
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { defineConfig } from "vite";
+import tsConfigPaths from "vite-tsconfig-paths";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   server: {
@@ -9,9 +10,10 @@ export default defineConfig({
   },
   plugins: [
     tsConfigPaths({
-      projects: ['./tsconfig.json'],
+      projects: ["./tsconfig.json"],
     }),
     tanstackStart(),
     viteReact(),
+    tailwindcss(),
   ],
-})
+});
